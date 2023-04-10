692e5a549ee91a6669ffa37ec2f89f9a502eae6e
Emilse Arias <emilsearias@gmail.com>
2023-04-10 11:10:15 -0300

    probamos el evento click en el boton editar

--- js/administrador.js
+++ js/administrador.js
@@ -1,5 +1,11 @@
 import Pelicula from "./classPelicula.js";
 
+const btnEditar = document.querySelector('#btnEditar');
+btnEditar.addEventListener('click', crearPeli);
+
+function crearPeli(){
 // crear una nueva peli
 let nuevaPeli = new Pelicula('Super mario','algo','url','aventura',2023, '2hs','EEUU','-');
 console.log(nuevaPeli)
+
+}
