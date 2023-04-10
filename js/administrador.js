e20bb632630bd95ad722c5bbf8996ac87ec7234b
Emilse Arias <emilsearias@gmail.com>
2023-04-10 11:34:56 -0300

    realizamos una instancia de la ventana modal de bootstrap desde js para manejar sus eventos show y hide

--- js/administrador.js
+++ js/administrador.js
@@ -1,7 +1,14 @@
 import Pelicula from "./classPelicula.js";
 
 const btnEditar = document.querySelector('#btnEditar');
+const btnAgregar = document.querySelector('#btnAgregar');
+const formularioPelicula = document.getElementById('formAdministrarPelicula');
+
 btnEditar.addEventListener('click', crearPeli);
+btnAgregar.addEventListener('click', mostrarModalPeli);
+formularioPelicula.addEventListener('submit', cargarPelicula);
+
+const modalPelicula = new bootstrap.Modal(document.querySelector('#modalAgregar'));
 
 function crearPeli(){
 // crear una nueva peli
@@ -9,3 +16,18 @@ let nuevaPeli = new Pelicula('Super mario','algo','url','aventura',2023, '2hs','
 console.log(nuevaPeli)
 
 }
+
+function mostrarModalPeli(){
+    //abrir la ventana modal
+    modalPelicula.show();
+    console.log('aqui vamos a crear una peli')
+}
+
+function cargarPelicula(e){
+    e.preventDefault();
+    console.log('creando la pelicula...')
+    //crear modal
+    modalPelicula.hide();
+}
+
+// tarea agregar validaciones HTML
